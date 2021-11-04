b8726c2043d7af4679db965f7271afe96c089004
kvammee <elliott.kvamme@gmail.com>
2021-11-04 16:38:36 -0500

    finished notes functions

---
+++ __tests__/notes.test.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const {
+    findById,
+    createNewNote,
+    updateNote,
+    deleteNote,
+    validateNote
+} = require('../lib/notes.js');
+const { notes } = require("../db/db.json");
+
+jest.mock('fs');
+
+// findById
+
+
+
+// createNewNote
+
+
+// updateNote
+
+
+// deleteNote
+
+
+// validateNote
